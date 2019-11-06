c01d98c2d7b7419cc95885bae7cd6efa30f4496b
masterUNG <phrombutr@gmail.com>
2019-11-06 15:21:14 +0700

    Design UI home Success

---
+++ lib/screens/home.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:ungpinthong/utility/my_style.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // Field
+
+  // Method
+
+  Widget signInButton() {
+    return Expanded(
+      child: RaisedButton(
+        shape: MyStyle().curveButton,
+        color: MyStyle().textColor,
+        child: Text(
+          'Sign In',
+          style: MyStyle().myWhiteTextStyle,
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget signUpButton() {
+    return Expanded(
+      child: OutlineButton(
+        shape: MyStyle().curveButton,
+        borderSide: BorderSide(color: MyStyle().textColor),
+        child: Text(
+          'Sign Up',
+          style: TextStyle(color: MyStyle().textColor),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      width: 250.0,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          signInButton(),
+          SizedBox(
+            width: 4.0,
+          ),
+          signUpButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget userText() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        decoration: InputDecoration(labelText: 'User :'),
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(labelText: 'Password :'),
+      ),
+    );
+  }
+
+  Widget showLogo() {
+    return Container(
+      width: 120.0,
+      height: 120.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
+  Widget showAppName() {
+    return Text(
+      'Ung Pinthong',
+      style: TextStyle(
+        fontSize: MyStyle().h1,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+        color: MyStyle().textColor,
+        fontFamily: MyStyle().nameFont,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/wallpaper.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                showLogo(),
+                showAppName(),
+                userText(),
+                passwordText(),
+                SizedBox(
+                  height: 8.0,
+                ),
+                showButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utility/my_style.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyStyle {
+
+  double h1 = 30.0, h2 = 18.0;
+  Color textColor = Colors.red[900];
+  Color mainColor = Colors.orange[600];
+  String nameFont = 'YeonSung';
+  String nameApp = 'Ung Pinthong';
+
+  TextStyle myWhiteTextStyle = TextStyle(color: Colors.white);
+
+  ShapeBorder curveButton = RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0),);
+
+  MyStyle();
+
+  
+}
